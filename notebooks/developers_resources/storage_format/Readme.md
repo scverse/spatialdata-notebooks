9e2adc61f7a0eb2c086a81d39af31c43aeba0ee4
Luca Marconato <m.lucalmer@gmail.com>
2023-08-23 19:23:47 +0200

    index.html

--- notebooks/developers_resources/storage_format/Readme.md
+++ notebooks/developers_resources/storage_format/Readme.md
@@ -14,7 +14,7 @@ This directory contains notebooks that operate on lightweight datasets.
   - `autorun: storage format; spatialdata from al29fak`
   - `autorun: storage format; spatialdata from fa096da (v0.0.12)`
 - The `.zarr` data produced by every run is available in the current directory, in the commit corresponding to the run.
-- The data is also [uploaded to S3](https://s3.embl.de/spatialdata/developers_resources/storage_format).
+- The data is also [uploaded to S3](https://s3.embl.de/spatialdata/developers_resources/storage_format/index.html), both as Zarr directories and as zipped files.
 
 ## How to use this repository
 Practically, a third party tool (e.g. R reader, format converter, JavaScript data visualizer, etc.) that runs correctly on the lightweight datasets from this repository, should be guaranteed to run correctly on any SpatialData dataset.
--- notebooks/developers_resources/storage_format/io_utils.py
+++ notebooks/developers_resources/storage_format/io_utils.py
@@ -4,6 +4,7 @@ import os
 import zarr
 from ome_zarr.io import parse_url
 import tempfile
+import shutil
 
 def delete_old_data(name: str) -> None:
     f1 = f'{name}.zarr'
@@ -55,3 +56,43 @@ def write_sdata_and_check_consistency(sdata: sd.SpatialData, name: str) -> None:
         f2 = os.path.join(tmpdir, f'{name}2.zarr')
         sdata2.write(f2)
         assert _are_directories_identical(f1, f2)
+
+    shutil.make_archive(f1, 'zip', f1)
+
+def make_index_html(elements: list[str]) -> None:
+    # Define the beginning of the HTML string
+    html = """
+    <html>
+    <head>
+    <title>Index</title>
+    </head>
+    <body>
+    <table border="1">
+    <tr>
+    <th>Element</th>
+    <th>Link</th>
+    <th>Zip</th>
+    </tr>
+    """
+
+    BASE_URL = 'https://s3.embl.de/spatialdata/developers_resources/storage_format/'
+    
+    # Populate the HTML string with table data
+    for element in elements:
+        html += f"<tr><td>{element}</td><td><a href='{BASE_URL}{element}.zarr'>{element}.zarr</a></td><td><a href='{BASE_URL}{element}.zarr.zip'>{element}.zarr.zip</a></td></tr>\n"
+    
+    # Close the HTML string
+    html += """
+    </table>
+Note: opening the above .zarr URLs in a web browser would not work, you need to treat the URLs as Zarr stores. For example if you append `/.zgroup` to any of the .zarr URLs above you will be able to see that file.
+    </body>
+    </html>
+    """
+    
+    # Write the HTML string to a file
+    with open("index.html", "w") as file:
+        file.write(html)
+
+if __name__ == '__main__':
+    notebooks = [f.replace('.ipynb', '') for f in os.listdir() if f.endswith('.ipynb') and f != '__template__.ipynb']
+    make_index_html(notebooks)
