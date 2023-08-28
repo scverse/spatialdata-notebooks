d339b1d5fe39867c4f592d8c52f0bcc5b8ef8964
Rahul Shrestha <rahulshrestha0101@gmail.com>
2023-08-28 16:32:12 +0200

    fix tests

--- tests/test_run_notebook.py
+++ tests/test_run_notebook.py
@@ -3,7 +3,7 @@ from run_notebook import add_parameters_to_cell
 
 def test_add_parameters_to_cell():
     cell_text = "Interactive(sdata=sdata, headless=True)"
-    expected_text = "Interactive(sdata=sdata, headless=True, coordinate_system_name=global, _tested_notebook=test_notebook.ipynb, _test_target=cell5, _take_screenshot=True)"
+    expected_text = 'Interactive(sdata=sdata, headless=True, coordinate_system_name="global", _tested_notebook="test_notebook.ipynb", _test_target="cell5", _take_screenshot=True)'
 
     assert add_parameters_to_cell(cell_text, take_screenshot=True) == expected_text
 
@@ -17,13 +17,13 @@ def test_add_parameters_to_cell_no_interactive():
 
 def test_add_parameters_to_cell_take_screenshot_is_false():
     cell_text = "Interactive(sdata=sdata, headless=True)"
-    expected_text = "Interactive(sdata=sdata, headless=True, coordinate_system_name=global, _tested_notebook=test_notebook.ipynb, _test_target=cell5, _take_screenshot=False)"
+    expected_text = 'Interactive(sdata=sdata, headless=True, coordinate_system_name="global", _tested_notebook="test_notebook.ipynb", _test_target="cell5", _take_screenshot=False)'
 
     assert add_parameters_to_cell(cell_text, take_screenshot=False) == expected_text
 
 
 def test_add_parameters_to_cell_screenshot_overwrite():
-    cell_text = "Interactive(sdata=sdata, headless=True, coordinate_system_name=global, _tested_notebook=test_notebook.ipynb, _test_target=cell5, _take_screenshot=False)"
-    expected_text = "Interactive(sdata=sdata, headless=True, coordinate_system_name=global, _tested_notebook=test_notebook.ipynb, _test_target=cell5, _take_screenshot=True)"
+    cell_text = 'Interactive(sdata=sdata, headless=True, coordinate_system_name="global", _tested_notebook="test_notebook.ipynb", _test_target="cell5", _take_screenshot=False)'
+    expected_text = 'Interactive(sdata=sdata, headless=True, coordinate_system_name="global", _tested_notebook="test_notebook.ipynb", _test_target="cell5", _take_screenshot=True)'
 
     assert add_parameters_to_cell(cell_text, take_screenshot=True) == expected_text
